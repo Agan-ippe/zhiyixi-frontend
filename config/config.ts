8feaca247be23b9e557b958c29ec487821057a4e
Aip <wjy7136@163.com>
2025-09-11 23:37:05 +0800

    chore(config/proxy.ts): 配置代理

--- config/config.ts
+++ config/config.ts
@@ -58,6 +58,13 @@ export default defineConfig({
    * @doc 代理配置 https://umijs.org/docs/api/config#proxy
    */
   proxy: proxy[REACT_APP_ENV as keyof typeof proxy],
+  // proxy: {
+  //   '/api': {
+  //     'target': 'http://jsonplaceholder.typicode.com/',
+  //     'changeOrigin': true,
+  //     'pathRewrite': { '^/api' : '' },
+  //   }
+  // },
   /**
    * @name 快速热更新配置
    * @description 一个不错的热更新组件，更新时可以保留 state
