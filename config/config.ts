6f3dce042ddedc5df614d34ff1045aa1da30e4af
Aip <wjy7136@163.com>
2025-09-12 19:18:09 +0800

    chore(src/locales): 移除国际化文件

--- config/config.ts
+++ config/config.ts
@@ -152,9 +152,6 @@ export default defineConfig({
   openAPI: [
     {
       requestLibPath: "import { request } from '@umijs/max'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      // schemaPath: join(__dirname, 'oneapi.json'),
       schemaPath: 'http://localhost:8101/api/v2/api-docs',
       projectName: 'zhibi',
       mock: false,
