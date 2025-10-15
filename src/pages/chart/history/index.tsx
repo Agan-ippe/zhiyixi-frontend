9c570473bb9ad69924c459d703884a86dbe72db4
Aip <wjy7136@163.com>
2025-10-15 22:13:04 +0800

    refactor(chart/history): 页面样式重构

--- src/pages/chart/history/index.tsx
+++ src/pages/chart/history/index.tsx
@@ -1,19 +1,8 @@
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
-import {
-  Card,
-  Empty,
-  Select,
-  Space,
-  Table,
-  type TableColumnsType,
-  TableProps,
-  Tag,
-} from 'antd';
-import React, { useState } from 'react';
+import { Card, Divider, List, message, Select } from 'antd';
+import Search from 'antd/es/input/Search';
+import ReactECharts from 'echarts-for-react';
+import React, { useEffect, useState } from 'react';
+import { listMyChartByPageUsingPost } from '@/services/zhibi/chartController';
 
 /**
  * 数据分析历史记录
@@ -22,107 +11,114 @@ import React, { useState } from 'react';
 const { Option } = Select;
 
 const chartHistory: React.FC = () => {
-  // const [chart, setChart] = useState<API.BiResponse>();
-  // const [submitting, setSubmitting] = useState<boolean>(false);
-  // const [option, setOption] = useState<any>();
-  // const { initialState } = useModel('@@initialState');
-  // const { currentUser } = initialState || {};
-  interface DataType {
-    key: string;
-    name: string;
-    age: number;
-    address: string;
-    tags: string[];
-  }
-
-  const columns: TableColumnsType<DataType>['columns'] = [
-    {
-      title: '图表名称',
-      dataIndex: 'chartName',
-      key: 'chartName',
-      description:
-        'My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.',
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      // 可筛选项
-      title: '图表类型',
-      dataIndex: 'chartType',
-      key: 'chartType',
-      filters: [
-        { text: '折线图', value: '折线图' },
-        { text: '柱状图', value: '柱状图' },
-        { text: '堆叠图', value: '堆叠图' },
-        { text: '雷达图', value: '雷达图' },
-        { text: '饼图', value: '饼图' },
-      ],
-      filterMode: 'tree',
-      filterSearch: true,
-      onFilter: (value, record) => record.name.startsWith(value as string),
-    },
-    {
-      title: '分析目标',
-      dataIndex: 'goal',
-      key: 'goal',
-    },
-    {
-      title: '结论',
-      key: 'genChartResult',
-      dataIndex: 'genChartResult',
-    },
-    {
-      title: '操作',
-      key: 'action',
-      render: (_, record) => (
-        <Space size="middle">
-          <a>查看</a>
-          <a>删除</a>
-        </Space>
-      ),
-    },
-  ];
-
-  // 模拟数据
-  const data: DataType[] = [
-    // {
-    //   key: '1',
-    //   name: 'John Brown',
-    //   age: 32,
-    //   address: 'New York No. 1 Lake Park',
-    //   tags: ['nice', 'developer'],
-    // },
-    // {
-    //   key: '2',
-    //   name: 'Jim Green',
-    //   age: 42,
-    //   address: 'London No. 1 Lake Park',
-    //   tags: ['loser'],
-    // },
-    // {
-    //   key: '3',
-    //   name: 'Joe Black',
-    //   age: 32,
-    //   address: 'Sydney No. 1 Lake Park',
-    //   tags: ['cool', 'teacher'],
-    // },
-  ];
-
-  const [loading, setLoading] = useState<boolean>(true);
+  // 图表查询初始化请求参数
+  const initSearchParams = {
+    current: 1,
+    pageSize: 4,
+  };
+  const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({
+    ...initSearchParams,
+  });
+  const [chartList, setChartList] = useState<API.ChartDO>();
+  // 展示条数
+  const [total, setTotal] = useState<number>(0);
+  const [searchLoading, setSearchLoading] = useState<boolean>(true);
+  // 从后端拿到历史数据/
+  const historyData = async () => {
+    setSearchLoading(true);
+    try {
+      const res = await listMyChartByPageUsingPost(searchParams);
+      if (res.data) {
+        setChartList(res.data.records ?? []);
+        setTotal(res.data.total ?? 0);
+        // 清除图表标题
+        if (res.data.records) {
+          res.data.records.forEach((data) => {
+            const chartOption = JSON.parse(data.genChartInfo ?? '{}');
+            chartOption.title = undefined;
+            data.genChartInfo = JSON.stringify(chartOption);
+          });
+        }
+      }
+    } catch (e: any) {
+      message.error('获取历史图表失败' + e.message);
+    }
+    setSearchLoading(false);
+  };
+  // 钩子函数，在访问这个页面前先请求后端拿到数据
+  useEffect(() => {
+    historyData();
+  }, [searchParams]);
 
   return (
     <div className="chart-history">
-      <Card title="历史记录">
-        <Table<DataType>
-          columns={columns}
-          expandable={{
-            expandedRowRender: (record) => (
-              <p style={{ margin: 0 }}>{record.description}</p>
-            ),
-            rowExpandable: (record) => record.name !== 'Not Expandable',
+      {/*搜索栏*/}
+      <div>
+        <Search
+          placeholder="请输入你要查找的图表名称"
+          loading={searchLoading}
+          enterButton
+          onSearch={(value) => {
+            setSearchParams({
+              // 每次搜索之前先重置搜索条件
+              ...initSearchParams,
+              // 加上搜索条件对应的属性
+              chartName: value,
+            });
           }}
-          dataSource={data}
         />
-      </Card>
+      </div>
+      {/*原子css*/}
+      <div className="margin-16"></div>
+      {/*数据展示*/}
+      <List
+        itemLayout="vertical"
+        // 分页组件
+        pagination={{
+          onChange: (page, pageSize) => {
+            setChartList(undefined);
+            setSearchParams({
+              ...searchParams,
+              current: page,
+              pageSize,
+            });
+          },
+          current: searchParams.current,
+          pageSize: searchParams.pageSize,
+          total: total,
+        }}
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 1,
+          lg: 2,
+          xl: 2,
+          xxl: 3,
+        }}
+        dataSource={chartList}
+        renderItem={(item) => (
+          <List.Item
+            key={item.chartId}
+            // actions={[<a>编辑</a>, <a>删除</a>]}
+          >
+            <Card>
+              <List.Item.Meta
+                title={item.chartName ?? '暂无名称'}
+                description={
+                  '分析诉求：' +
+                  item.goal +
+                  '，并使用 “' +
+                  item.chartType +
+                  '” 展示数据'
+                }
+              />
+              {item.genChartResult}
+              <ReactECharts option={JSON.parse(item.genChartInfo ?? '{}')} />
+            </Card>
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
